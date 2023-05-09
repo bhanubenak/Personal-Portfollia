2adc4dee6bb67ecb94d6d030007f86ca0ac7c253
bhanubenak <58963477+bhanubenak@users.noreply.github.com>
2023-05-09 11:36:04 +0530

    Update script.js

---
+++ src/script.js
@@ -0,0 +1,20 @@
+// menu show
+
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+// menu show
+//  validate if constant exists
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+// menu hidden
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
