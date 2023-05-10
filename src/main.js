db08f5aba49b81416110f0eb0ad57f8b98917497
bhanubenak <58963477+bhanubenak@users.noreply.github.com>
2023-05-10 22:16:27 +0530

    update index.html, style.css & script.js

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ const navMenu = document.getElementById('nav-menu'),
 if (navToggle) {
     navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
+        console.log("show menu")
     })
 }
 
